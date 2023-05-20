1fbb72d9f6b15fe7c929e3014f59bd5e248a88bb
ashesh <ashesh276@gmail.com>
2023-05-20 21:44:48 +0000

    init() updated

--- deconoising/training.py
+++ deconoising/training.py
@@ -417,9 +417,9 @@ def trainNetwork(net,
     valHist: numpy array
         A numpy array containing the avg. validation loss after each epoch.
     '''
-    # wandb.init()
     exptname = '/'.join(workdir.strip('/').split('/')[-3:])
     hostname = socket.gethostname()
+    wandb.init(name=os.path.join(hostname, exptname), dir=workdir, project="Multi-PSF-Deconoising")
     logger = WandbLogger(name=os.path.join(hostname, exptname), save_dir=workdir, project="Multi-PSF-Deconoising")
     if psf_learnable:
         # Create a list of learnable gaussian kernels.
